dfa75d49ef81e747692cb3a3cdc0f5ac18bde444
Teucrian <itveiizandr@gmail.com>
2024-11-29 00:52:57 -0800

    feat: X_MIN_POS -45 Y_MIN_POS -5

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1909,7 +1909,7 @@
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS -45
-#define Y_MIN_POS -7
+#define Y_MIN_POS -5
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
