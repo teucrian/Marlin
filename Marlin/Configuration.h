6b4dffbbcaeb9bd34e93dd3c524b7ba9291634fb
Teucrian <itveiizandr@gmail.com>
2024-12-02 16:40:21 -0800

    feat: X_MIN_POS -46

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1,4 +1,4 @@
-/**
+ /**
  * Marlin 3D Printer Firmware
  * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
@@ -1908,7 +1908,7 @@
 #define Y_BED_SIZE 216
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -45
+#define X_MIN_POS -46
 #define Y_MIN_POS -6
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
